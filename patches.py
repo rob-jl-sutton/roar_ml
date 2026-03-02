1bb7920105ca4bb18b778525594e530e0ac2ed8e
Sutton <Robert.Sutton@uhs.nhs.uk>
2026-03-02 16:43:30 +0000

    Update to patches with initial loop through cases

--- patches.py
+++ patches.py
@@ -4,17 +4,21 @@
 #
 #
 #0.0 import modules
-
-
-
-
-#1.0 specify location of files
-
+import os.path
+import glob
 
 #2.0 perform recursive search to identify available roi files
+matches = glob.glob("D:/**/*roi.nrrd", recursive = True)
+ids = [match[3:9] for match in matches]
+ids_unique = list(set(ids))
+print(ids_unique)
+os.makedirs(r"C:\Documents\ROAR workflow 6.0\volumes_patches", exist_ok=True)
 
-
+"C:\Users\suttor\Documents\ROAR workflow 6.0"
 #3.0 specify patch size and adjust number of patches if required
+
+
+#recommended patch density
 
 
 #4.0 save to single output file training_patches
