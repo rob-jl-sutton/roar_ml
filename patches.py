02bb780d8b849a9b8a558287470d0e0f80561270
Sutton <Robert.Sutton@uhs.nhs.uk>
2026-03-02 14:22:09 +0000

    Add patches - file to break segmentations into fixed patch size for training

---
+++ patches.py
@@ -0,0 +1,20 @@
+#author:rob-jl-sutton
+#purpose of script is to generate
+#patches for ML training of aneurysm segmentation
+#
+#
+#
+
+
+
+
+#1.0 specify location of files
+
+
+#2.0 perform recursive search to identify available roi files
+
+
+#3.0 specify patch size and adjust number of patches if required
+
+
+#4.0 save to single output file training_patches
